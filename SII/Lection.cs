196acc644d6ef6a711c0a3f195ed3d729f647170
enfiol <enfiol@mail.ru>
2020-11-05 14:05:26 +0300

    Initial commit

---
+++ SII/Lection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProximityMeasures
+{
+    public class Lection
+    {
+        public string Title { get; set; }
+        public string University { get; set; } 
+        public string Country { get; set; }
+        public string Subject { get; set; }
+        public string Language { get; set; } 
+        public string Author { get; set; } 
+        public double Rating { get; set; } 
+        public int Pages { get; set; }
+        public int Year { get; set; }
+        public int ThemesCount { get; set; }
+
+        //public int ReadTime { get; set; } //???
+        //public string Maintheme { get; set; }
+        //public bool Status { get; set; } //true - complete
+    }
+}
